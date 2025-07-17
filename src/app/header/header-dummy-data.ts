c9d1bfaa8ce2947d4007ccc10e93426d2bd53c46
ymercadov <yusethmercado@gmail.com>
2025-07-16 22:47:35 -0500

    Cabecera responsive añadida - Fase #3

---
+++ src/app/header/header-dummy-data.ts
@@ -0,0 +1,60 @@
+
+export const lenguages = [
+    {
+        lenguage: 'English',
+        flag: 'us'
+    },
+    {
+        lenguage: 'French',
+        flag: 'france'
+    },
+    {
+        lenguage: 'German',
+        flag: 'germany'
+    },
+    {
+        lenguage: 'Russian',
+        flag: 'russia'
+    },
+    {
+        lenguage: 'Spanish',
+        flag: 'spain'
+    }
+];
+
+export const notifications = [
+    {
+        icon: 'far fa-cloud-download',
+        subject: 'Donload complete',
+        description: 'Lorem ipsum dolor sit amet, consectetuer.',        
+    },
+    {
+        icon: 'far fa-cloud-upload',
+        subject: 'Upload complete',
+        description: 'Lorem ipsum dolor sit amet, consectetuer.',        
+    },
+    {
+        icon: 'far fa-trash',
+        subject: '350 MB trash files',
+        description: 'Lorem ipsum dolor sit amet, consectetuer.',        
+    }
+];
+
+export const userItems = [
+    {
+        icon:'far fa-user',
+        label:'Profile'
+    },
+    {
+        icon:'far fa-cog',
+        label:'Settings'
+    },
+    {
+        icon:'far fa-unlock-alt',
+        label:'Lock screen'
+    },
+    {
+        icon:'far fa-power-off',
+        label:'Logout'
+    }
+]
